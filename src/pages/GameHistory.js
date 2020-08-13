c984bb5a9b5bae91c701d11e65fbde94131192d7
jalmansa <jalmansa88@gmail.com>
2020-08-13 20:20:24 +0200

    pages scaffolding

---
+++ src/pages/GameHistory.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Title } from '../components/Title';
+
+export class GameHistory extends Component {
+	render() {
+		return (
+			<div>
+				<Title>Game History</Title>
+			</div>
+		);
+	}
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Title } from '../components/Title';
+
+export class Home extends Component {
+	render() {
+		return (
+			<div>
+				<Title>Rock, Paper, Scissors WebApp Game</Title>
+			</div>
+		);
+	}
+}
